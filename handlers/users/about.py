a08411e6803d72dedafa24ae000cabb8be331faa
Nurbek333 <nurbekuktamov333@gmail.com>
2024-08-27 18:57:26 +0500

    com

--- handlers/users/about.py
+++ handlers/users/about.py
@@ -5,5 +5,28 @@ from aiogram.filters import Command
 #about commands
 @dp.message(Command("about"))
 async def about_commands(message:Message):
-    await message.answer("Sifat 2024")
+    user_id = message.from_user.id
+    referal_link = f"https://t.me/dwefe_bot?start={user_id}"
+    response_text = ("<b>🤖 Bot haqida:</b>\n"
+
+    "<b>Ushbu bot sizga va do'stlaringizga o'yin yoki xizmatlar uchun ball to'plash imkonini beradi. Botdan foydalanish orqali turli imtiyozlarga ega bo'lishingiz mumkin! 🎉</b>\n"
+
+    "<b>🎯 Referal tizimi qanday ishlaydi?</b>\n"
+
+    "1. <b>👤 1-daraja: Sizning to'g'ridan-to'g'ri referallaringiz. Ular har bir harakatda sizga ball olib keladi.</b>\n"
+    "2. <b>👥 2-daraja: Sizning referallaringizning referallari. Ularning harakatlari uchun ham siz ball to'playsiz.</b>\n"
+    "3. <b>👨‍👩‍👧‍👦 3-daraja: 3-darajali referallar orqali ham ball yig'ishingiz mumkin.</b>\n"
+
+    "<b>🔗 Referal havolangizni do'stlaringiz bilan ulashing va ko'proq ball to'plang!</b>\n"
+
+    f"<b>Sizning shaxsiy referal havolangiz:</b> <code>{referal_link}</code>\n"
+
+    "<b>📊 Ballaringizni kuzatib boring:</b> `Menign ballarim`\n"
+
+    "<b>🆘 Yordam kerak bo'lsa:</b> `/help`\n"
+
+    "<b>💼 Admin bilan bog'lanish:</b> @admin_username\n"
+
+    "<b>🔥 Omad tilaymiz!</b>")
+    await message.answer(text=response_text, parse_mode="html")
 
--- handlers/users/help.py
+++ handlers/users/help.py
@@ -6,4 +6,29 @@ from aiogram.filters import Command
 #help commands
 @dp.message(Command("help"))
 async def help_commands(message:Message):
-    await message.answer("Sizga qanday yordam kerak")
+    help_text = """
+<b>🆘 Yordam:</b>    
+
+Botimiz qanday ishlashini tushuntiruvchi ko'rsatmalar:    
+
+1. <b>🎯 Referal tizimi:</b>    
+  - Do'stlaringizni taklif qiling va ball to'plang.    
+  - Referal havolangizni olish uchun /referal buyrug'ini bosing.     
+
+2. <b>📊 Ballaringizni kuzatish:</b>    
+  - O'zingiz to'plagan ballaringizni ko'rish uchun /my_points buyrug'ini yozing.     
+
+3. <b>💬 Yordam kerakmi?</b>
+  - Qo'shimcha yordam olish uchun <a href="https://t.me/admin_username">Admin bilan bog'laning</a>.         
+
+<b>Qo'llab-quvvatlash:</b> Botda duch kelgan muammolaringiz yoki savollaringiz bo'lsa, yuqoridagi admin bilan bog'lanishingiz mumkin.     
+
+<b>🔗 Foydali buyruqlar:</b>    
+  - /start: Botni qayta ishga tushirish.  
+  - /referal: Referal havolangizni olish.  
+  - /about: Bot haqida ma'lumot olish.  
+  - /my_points: Ballaringizni ko'rish.        
+
+<b>🔥 Omad tilaymiz!</b>    
+    """
+    await message.answer(help_text, parse_mode="html")
